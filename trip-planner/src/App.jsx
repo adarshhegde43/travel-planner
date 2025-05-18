6ed7618cec4f6be01bdf852899b1db73db06117d
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-05-18 15:04:05 +0530

    1.2 - Created landing page

--- trip-planner/src/App.jsx
+++ trip-planner/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import Hero from './components/custom/Hero'
 import { Button } from './components/ui/button'
 
 function App() {
 
   return (
-    <Button className="bg-blue-600 hover:bg-blue-700 text-white">
-      Click me
-    </Button> 
+    <div className='flex flex-col items-center mx-56 gap-9'>
+      <h1
+        className='font-extrabold text-[45px] text-center mt-16'
+        >
+        <span className='text-[#f56551]'> Discover Your Next Adventure with AI :</span></h1>
+      <h1
+        className='font-extrabold text-[45px] text-center mt-0'
+        >
+        <span> Personal Itineraries at Your Fingertips </span></h1>
+        <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator , creating custom itineraries tailored to your interests and budget.</p>
+
+        <Button> Get Started , It's Free</Button>
+    </div>
   )
 }
 
