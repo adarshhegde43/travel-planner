463a349228ce01f767749419e1a91d5759b6da71
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-05-18 13:40:37 +0530

    1.1 - Project has been setup , dependencies installed

---
+++ trip-planner/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <Button className="bg-blue-600 hover:bg-blue-700 text-white">
+      Click me
+    </Button> 
+  )
+}
+
+export default App
